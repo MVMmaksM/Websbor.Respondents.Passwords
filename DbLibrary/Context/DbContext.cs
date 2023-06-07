ce4be2817408e26d10de6eecf0a5b5e6bdad9d14
MVMmaksM <vaganov.maksim94@yandex.ru>
2023-06-07 16:15:17 +0500

    Create PasswordRepositories

---
+++ DbLibrary/Context/DbContext.cs
@@ -0,0 +1,22 @@
+﻿using DbLibrary.Configurations;
+using DbLibrary.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbLibrary.Context
+{
+    public class DbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public DbSet<Passwords> Passwords { get; set; }
+        public DbSet<WebsborGS> WebsborGS { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(AppSettings.ConnectionString);
+        }
+    }
+}
---
+++ DbLibrary/Repositories/PasswordsRepositories.cs
@@ -0,0 +1,123 @@
+﻿using DbLibrary.Context;
+using DbLibrary.Model;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DbContext = DbLibrary.Context.DbContext;
+
+namespace DbLibrary.Repositories
+{
+    public class PasswordsRepositories
+    {
+        private static string sqlStoredProcedure = "sp_Insert_Passwords @name @okpo @password @date_create @comment";
+        public List<Passwords> GetAll()
+        {
+            using (var dbContext = new DbContext())
+            {
+                return dbContext.Passwords.ToList();
+            }
+        }
+
+        public Passwords GetById(int id)
+        {
+            using (var dbContext = new DbContext())
+            {
+                return dbContext.Passwords.FirstOrDefault(p => p.Id.Equals(id));
+            }
+        }
+
+        public List<Passwords> GetByName(string name)
+        {
+            using (var dbContext = new DbContext())
+            {
+                return dbContext.Passwords.Where(p => p.Name.Equals(name)).ToList();
+            }
+        }
+        public List<Passwords> GetByOkpo(string okpo)
+        {
+            using (var dbContext = new DbContext())
+            {
+                return dbContext.Passwords.Where(p => p.Name.Equals(okpo)).ToList();
+            }
+        }
+
+        public async Task<int> AddAsync(Passwords dataRespondent)
+        {
+            using (var dbContext = new DbContext())
+            {
+                dbContext.Passwords.Add(dataRespondent);
+                return await dbContext.SaveChangesAsync();
+            }
+        }
+
+        public async Task<int> Update(Passwords dataRespondentUpdate) 
+        {
+            using (var dbContext = new DbContext())
+            {
+                dbContext.Passwords.Update(dataRespondentUpdate);
+                return await dbContext.SaveChangesAsync();
+            }
+        }
+
+        public async Task<int> Delete(Passwords dataRespondentDelete) 
+        {
+            using (var dbContext = new DbContext())
+            {
+                dbContext.Passwords.Remove(dataRespondentDelete);
+                return await dbContext.SaveChangesAsync();
+            }
+        }
+
+        public void AddStoredProcedure(List<Passwords> dataLoadRespondents) 
+        {
+            var sqlParameters = new List<SqlParameter>();
+
+            foreach (var data in dataLoadRespondents)
+            {
+                sqlParameters.Add(new SqlParameter
+                {
+                    ParameterName = "@name",
+                    Value = data.Name
+                });
+
+                sqlParameters.Add(new SqlParameter
+                {
+                    ParameterName = "@okpo",
+                    Value = data.Okpo
+                });
+
+                sqlParameters.Add(new SqlParameter
+                {
+                    ParameterName = "@password",
+                    Value = data.Password
+                });
+
+                sqlParameters.Add(new SqlParameter
+                {
+                    ParameterName = "@password",
+                    Value = data.Password
+                });
+
+                sqlParameters.Add(new SqlParameter
+                {
+                    ParameterName = "@date_create",
+                    Value = data.DateCreate
+                });
+
+                sqlParameters.Add(new SqlParameter
+                {
+                    ParameterName = "@comment",
+                    Value = data.Comment
+                });
+            }
+
+            using (var dbContext = new DbContext())
+            {                            
+                dbContext.Passwords.FromSqlRaw(sqlStoredProcedure, sqlParameters);
+            }
+        }
+    }
+}
