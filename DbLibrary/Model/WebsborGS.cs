78f54eb37b0ba69e48ba3b589a1d83f90502d8ff
MVMmaksM <vaganov.maksim94@yandex.ru>
2023-06-08 09:48:07 +0500

    Add prop Inn websborGs

--- DbLibrary/Model/WebsborGS.cs
+++ DbLibrary/Model/WebsborGS.cs
@@ -12,6 +12,7 @@ namespace DbLibrary.Model
         public string NameGS { get; set; }
         public string ShortNameGS { get; set; }
         public string OkpoGS { get; set; }
+        public string Inn { get; set; }
         public string OkpoUlGS { get; set; }
         public string AddressGS { get; set; }
         public string? TelephoneGS { get; set; }
